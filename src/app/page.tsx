b7e1fcc6e63c81f1193e69bdb88870f8bfca268f
Edwin Andermyr <andermyredwin@gmail.com>
2025-01-13 17:32:04 +0100

    adjusted rag lvl 3 title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="h-screen w-screen flex flex-col max-w-[80%] mx-auto bg-gray-800">
       <h1 className="text-2xl font-bold text-center my-8 text-white">
-        Claude Anthropic Rag Level 2 Calls
+        Claude Anthropic Rag Level 3 Calls
       </h1>
       <Chat />
     </main>
